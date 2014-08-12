1dfb74b037a3bbd61af9be1ca83f96e6c46d4639
hisato tanaka <hisato.tanaka@gmail.com>
2014-08-12 11:01:06 +0900

    constraints remove

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
 
-  #社内用トップメッセージ
-  constraints host: 'topmessages.yamaokaya.com' do
-    get '/wb/:type/:key(.:format)' => 'topmessages#show',as: :top_messages
-  end
-  constraints host: 'topmessages2.yamaokaya.com' do
-    get '/wb/:type/:key(.:format)' => 'topmessages#show',as: :top_messages2
-  end
+  get '/wb/:type/:key(.:format)' => 'topmessages#show',as: :top_messages
+  get 'cb/use/:coupon_code/:no(.:format)' => 'deliver#use',as: :use_coupon
 
-  #クーポン配信
-  constraints host: 'deliver.coupon.yamaokaya.com' do
-    get 'cb/use/:coupon_code/:no(.:format)' => 'deliver#use'
-  end
-  constraints host: 'deliver2.coupon.yamaokaya.com' do
-    get 'cb/use/:coupon_code/:no(.:format)' => 'deliver#use'
-  end
 end
